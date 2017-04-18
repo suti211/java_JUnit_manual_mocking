6cf816c0d268c9b5a73d0a10e1f7023b87d29b93
Unknown <suti211@gmail.com>
2017-04-18 17:02:38 +0200

    Implemented FileWordAnalyzer

---
+++ src/statistics/FileWordAnalyzer.java
@@ -0,0 +1,44 @@
+package statistics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import io.FilePartReader;
+
+public class FileWordAnalyzer {
+	FilePartReader partReader;
+
+	public FileWordAnalyzer(FilePartReader partReader) {
+		this.partReader = partReader;
+	}
+
+	public ArrayList wordsByAbc() {
+		ArrayList<String> sortedWords = new ArrayList<>();
+		
+		for(String word : getStrippedFilePart()){
+			sortedWords.add(word);
+		}
+		
+		//Collections.sort(sortedWords, String.CASE_INSENSITIVE_ORDER);
+		sortedWords.sort(String::compareToIgnoreCase);
+		
+		return sortedWords;
+	}
+
+	public ArrayList wordsContatiningSubstring(String substring) {
+		ArrayList<String> containingWords = new ArrayList<>();
+		
+		for(String word : getStrippedFilePart()){
+			if(word.contains(substring)){
+				containingWords.add(word);
+			}
+		}
+		return containingWords;
+	}
+	
+	private String[] getStrippedFilePart(){
+		String filePart = partReader.readLines();
+		String[] wordsInPart = filePart.replaceAll("\\s", " ").split(" ");
+		return wordsInPart;
+	}
+}
